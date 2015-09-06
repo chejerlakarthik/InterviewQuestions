e2e2b8c692a422559044100a7e9b52c101c99a58
Chejerla Karthik <chejerlakarthik@gmail.com>
2015-09-06 14:13:28 +0530

    Added class B

---
+++ InterviewQuestions/src/org/javabrains/statictest/B.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.javabrains.statictest;
+
+/**
+ * @author Yashoda
+ *
+ */
+public class B extends A 
+{
+	static
+    {
+        i = --i - i--;
+    }
+ 
+    {
+        i = ++i + i++;
+    }
+}
